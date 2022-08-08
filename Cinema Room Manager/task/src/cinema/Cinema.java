7090d42ddfae720c547389074ffd796841ae44ae
davidttpb <dalvarezpb@gmail.com>
2022-08-07 21:15:00 -0500

    Added the total profit for a cinema

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -1,4 +1,5 @@
 package cinema;
+import java.util.Scanner;
 
 public class Cinema {
 
@@ -15,6 +16,33 @@ public class Cinema {
         String s = " S S S S S S S S";
         for (int i = 1; i < rows; i++) {
             System.out.println(i + s);
+        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int numRows = scanner.nextInt();
+
+        System.out.println("Enter the number of seats in each row:");
+        int numSeats = scanner.nextInt();
+
+        if (numRows >= 1 && numRows <= 9 && numSeats >= 1 && numSeats <= 9) {
+            int totalSeats = numRows * numSeats;
+            if (totalSeats <= 60) {
+                int profitOfSmallRoom = totalSeats * 10;
+                System.out.println("Total income:");
+                System.out.print("$" + profitOfSmallRoom);
+            } else if (totalSeats > 60 && numRows % 2 == 0) {
+                int frontRow = numRows / 2;
+                int backRow = numRows / 2;
+                int totalIncome = frontRow * numSeats * 10 + backRow * numSeats * 8;
+                System.out.println("Total income:");
+                System.out.println("$" + totalIncome);
+            } else if (numRows % 2 == 1) {
+                int frontRow = numRows / 2;
+                int backRow = (numRows / 2) + 1;
+                int totalIncome = frontRow * numSeats * 10 + backRow * numSeats * 8;
+                System.out.println("Total income:");
+                System.out.println("$" + totalIncome);
+            }
         }
     }
 }
